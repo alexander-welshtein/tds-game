df2657a20e6afc90c7a585c636e11650752b08c4
Alexander Welshtein <alexanderwelshtein@gmail.com>
2020-08-08 15:39:16 +0400

    Fix client state update

--- src/manager.rs
+++ src/manager.rs
@@ -4,7 +4,7 @@ pub fn change_client_state(operation: Operation, state: &mut ClientState) {
     state.operation.id = operation.id;
     state.operation.command = operation.command;
 
-    match operation.command.as_str() {
+    match state.operation.command.as_str() {
         "MoveLeft" => state.player.x -= state.player.speed,
         "MoveRight" => state.player.x += state.player.speed,
         "MoveUp" => state.player.y -= state.player.speed,
